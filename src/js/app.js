391a94a4edfafa77aff93a85790ca1b54c1ea5cc
Suley <suleyliu@gmail.com>
2020-05-21 18:58:25 -0500

    click on one list it shows selected and other selected class removed.

--- src/js/app.js
+++ src/js/app.js
@@ -2,6 +2,10 @@ const originFormEle = document.querySelector(`.origin-form`);
 const destinationFormEle = document.querySelector(`.destination-form`);
 const originsUL = document.querySelector(`.origins`);
 const destinationsUL = document.querySelector(`.destinations`);
+let originLat;
+let originLon;
+let destinationLat;
+let destinationLon;
 
 const apikey = `pk.eyJ1Ijoic3VsYXlsaXUiLCJhIjoiY2thNWlrYmNnMDBpaDNsbm9lOHQ2MG5ncSJ9.iLbn-Tba_v8DH2S_ffwwDA`;
 
@@ -22,6 +26,33 @@ destinationFormEle.addEventListener(`submit`, function(event){
   }
   event.preventDefault();
 });
+
+originsUL.addEventListener(`click`, function(event) {
+  const lists = originsUL.querySelectorAll(`li`);
+  const clickedEle = event.target.closest('li');
+
+  lists.forEach((list) => {
+    if(list.classList.contains(`selected`)) {
+      list.classList.remove(`selected`);
+    }
+  })
+  clickedEle.classList.add(`selected`);
+  console.log(clickedEle);
+});
+
+destinationsUL.addEventListener(`click`, function(event) {
+  const lists = destinationsUL.querySelectorAll(`li`);
+  const clickedEle = event.target.closest('li');
+
+  lists.forEach((list) => {
+    if(list.classList.contains(`selected`)) {
+      list.classList.remove(`selected`);
+    }
+  })
+  clickedEle.classList.add(`selected`);
+  console.log(clickedEle);
+})
+
 
 function searchLocation(name, ULEle) {
   fetch(`https://api.mapbox.com/geocoding/v5/mapbox.places/${name}.json?bbox=-97.325875,49.766204,-96.953987,49.99275&access_token=${apikey}&limit=10`)
@@ -44,7 +75,7 @@ function getStreetsList(features, ULEle) {
   features.forEach(feature => {
     if (feature.properties.address !== undefined) {
       streetHTML +=  `
-        <li data-long="${feature.center[0]}" data-lat="${feature.center[1]}" class="selected">
+        <li data-long="${feature.center[0]}" data-lat="${feature.center[1]}">
           <div class="name">${feature.text}</div>
           <div>${feature.properties.address}</div>
         </li>`
